9a0901318c364d355576d8efe31b2ad89afc80a2
Chieloka Madubugwu <147743243+ChielokaCode@users.noreply.github.com>
2025-12-24 14:56:34 +0100

    Update azureConfig.ts

--- src/config/azureConfig.ts
+++ src/config/azureConfig.ts
@@ -13,26 +13,26 @@ const AZURE_AD_REDIRECT_URI = window.location.origin;
 export const API_BASE_URL =
   window.location.origin.includes('localhost')
     ? 'http://localhost:7071/api'
-    : 'https://my-backend.azurewebsites.net/api';
+    : 'https://ambitious-pond-0b778870f.1.azurestaticapps.net/api';
 
 export const azureConfig = {
   api: {
     baseUrl: API_BASE_URL,
     endpoints: {
-      getPhotos: `${API_BASE_URL}/api/get-photos`,
-      getPhoto: `${API_BASE_URL}/api/get-photo`,
-      createPhoto: `${API_BASE_URL}/api/create-photo`,
-      deletePhotoHandler: `${API_BASE_URL}/api/delete-photo`,
-      getComments: `${API_BASE_URL}/api/get-comments`,
-      createComment: `${API_BASE_URL}/api/create-comment`,
-      deleteComment: `${API_BASE_URL}/api/delete-comment`,
-      getUserLikedPhotos: `${API_BASE_URL}/api/get-user-liked-photos`,
-      likePhoto: `${API_BASE_URL}/api/like-photo`,
-      unlikePhoto: `${API_BASE_URL}/api/unlike-photo`,
-      getLikes: `${API_BASE_URL}/api/get-likes`,
-      signIn: `${API_BASE_URL}/api/signin`,
-      SignUpConsumer: `${API_BASE_URL}/api/signup-consumer`,
-      signUpCreator: `${API_BASE_URL}/api/signup-creator`,
+      getPhotos: `${API_BASE_URL}/get-photos`,
+      getPhoto: `${API_BASE_URL}/get-photo`,
+      createPhoto: `${API_BASE_URL}/create-photo`,
+      deletePhotoHandler: `${API_BASE_URL}/delete-photo`,
+      getComments: `${API_BASE_URL}/get-comments`,
+      createComment: `${API_BASE_URL}/create-comment`,
+      deleteComment: `${API_BASE_URL}/delete-comment`,
+      getUserLikedPhotos: `${API_BASE_URL}/get-user-liked-photos`,
+      likePhoto: `${API_BASE_URL}/like-photo`,
+      unlikePhoto: `${API_BASE_URL}/unlike-photo`,
+      getLikes: `${API_BASE_URL}/get-likes`,
+      signIn: `${API_BASE_URL}/signin`,
+      SignUpConsumer: `${API_BASE_URL}/signup-consumer`,
+      signUpCreator: `${API_BASE_URL}/signup-creator`,
     },
   },
   // storage: {
