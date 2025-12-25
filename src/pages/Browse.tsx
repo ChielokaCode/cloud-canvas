968f1ce2b78354373558b3fea799e7f6c0ce5310
Chieloka Madubugwu <147743243+ChielokaCode@users.noreply.github.com>
2025-12-25 10:39:59 +0100

    Update Browse.tsx

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -73,7 +73,7 @@ const Browse = () => {
           </p>
         </div>
 
-        {/* Filters */}
+        {/* Filterss */}
         <div className="flex flex-col sm:flex-row gap-4 mb-8">
           <div className="relative flex-1">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
